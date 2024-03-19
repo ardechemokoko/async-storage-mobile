9b5f62d6bab1e9b0ecc5050640630553cc5bc441
ardeche2000 <mokokoardeche@gmail.com>
2024-03-19 16:14:54 +0100

    suite projet

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './components/Home';
 import About from './components/About';
+import Login from './components/Login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          initialRouteName='Login'
+          screenOptions={{headerShown:false}}
+        >
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="About" component={About} />
       </Stack.Navigator>
---
+++ components/Login.js
@@ -0,0 +1,86 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { MaterialIcons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Login = ({navigation}) => {
+    const [name,setName]=useState(null);
+
+    const HandleSave = async() => {
+        if(name.length > 0){
+            try {
+                await AsyncStorage.setItem("myName",name)
+                navigation.navigate('Home');
+            } catch (error) {
+                alert(error);
+            }
+        }
+        else{
+            alert('Le nom est obligatoire !');
+        }
+
+    }
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <MaterialIcons name="facebook" size={80} color="white" />
+      </View>
+      <View style={styles.inputContianer}>
+        <Text>{name}</Text>
+        <TextInput
+            placeholder='Votre nom ' 
+            style={styles.input}
+            onChangeText={text=>setName(text)}
+        />
+        <TouchableOpacity 
+        onPress={HandleSave}
+        style={styles.touchable}>
+            <View style={styles.boutton}>
+                <Text style={styles.textBnt}>Se connecter</Text>
+            </View>
+        </TouchableOpacity>
+
+      </View>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#229EAD',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo:{
+        marginBottom:40,
+    },
+    inputContianer:{
+        width :"100%",
+        paddingHorizontal:50
+    },
+    input:{
+        backgroundColor :'#FFF',
+        borderRadius:25,
+        padding:9,
+        textAlign:'center',
+        fontSize:19
+    },
+    touchable:{
+        marginVertical:9
+    },
+    boutton:{
+        borderColor:'white',
+        borderWidth:1,
+        alignItems:'center',
+        justifyContent:'center',
+        padding:12,
+        borderRadius:25,
+        marginTop:20
+    },
+    textBnt:{
+        color:'white'
+    }
+})
